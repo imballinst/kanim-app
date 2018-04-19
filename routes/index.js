68fb0d70fce48b3138a900011c2f167e37e671cd
imballinst <try.aji@efishery.com>
2018-04-19 23:32:13 +0700

    feat(route): added route for kanim specific quota info

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const { ObjectId } = require('mongodb');
 const app = require('../app');
 // const { winstonInfo } = require('../lib/logging');
 const { find, insertOne, updateOne } = require('../lib/mongo');
+const { postAvailabilityInfo, postCheckSession } = require('../lib/requests');
+const { getOfficeQuota } = require('../lib/quota');
 
 module.exports = () => {
   app.get('/user/:userID/notification', (req, res) => {
@@ -52,5 +54,44 @@ module.exports = () => {
     )
       .then(({ data }) => res.send({ success: true, data }))
       .catch(err => res.send({ success: false, error: err }));
+  });
+
+  app.post('/quota/:moID', (req, res) => {
+    // get quota for an office
+    const { token, startDate, endDate } = req.body;
+    const response = { success: false };
+
+    res.set('Content-Type', 'application/json');
+
+    if (!token) {
+      response.error = 'Please provide token!';
+      res.send(response);
+    } else {
+      postCheckSession(undefined, token).then(({ data }) => {
+        const { Success, Message } = data;
+
+        if (Success) {
+          return postAvailabilityInfo(undefined, token, req.params.moID, startDate, endDate);
+        }
+
+        return { data: { Message, errorCode: 401 } };
+      }).then(({ data }) => {
+        const {
+          Success, Message, Availability, errorCode,
+        } = data;
+
+        if (!Message || Success) {
+          const quota = getOfficeQuota(Availability);
+
+          response.data = quota;
+          response.success = true;
+        } else {
+          response.error = Message;
+          response.errorCode = errorCode;
+        }
+
+        res.send(response);
+      });
+    }
   });
 };
