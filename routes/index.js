26e2a2bc24ea5b3239122065fdfdeb9decf14d98
imballinst <try.aji@efishery.com>
2018-06-19 18:59:34 +0700

    chore(wercker): test log

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+const { winstonInfo } = require('../lib/logging');
+const { NODE_ENV } = require('../config/env');
+
 const initNotifRoutes = require('./notifications');
 const initOfficesRoutes = require('./offices');
 const initQueueRoutes = require('./queue');
 const initAuthRoutes = require('./auth');
 
 module.exports = (app) => {
+  if (NODE_ENV !== 'test') {
+    app.use((req, res, next) => {
+      const { method, originalUrl, query, body } = req;
+      const data = method === 'GET' ? query : body;
+
+      // delete password logging
+      if (data && data.password) {
+        delete data.password;
+      }
+
+      winstonInfo(`[kanim-app] ${method} ${originalUrl} ${JSON.stringify(data)}`);
+      next();
+    });
+  }
+
   initNotifRoutes(app);
   initOfficesRoutes(app);
   initQueueRoutes(app);
