94c5573a0f99b484ca110eb412addfe1d64394a0
Try Ajitiono <ballinst@gmail.com>
2018-11-25 15:14:33 +1100

    Add register and forgot password API

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,98 @@
-const { postLogin } = require('../lib/request');
+const { postLogin, postRegister, postResetPassword } = require('../lib/request');
 const { parseJSONIfString } = require('../lib/objectUtil');
 
-module.exports = (app) => {
+module.exports = app => {
   app.post('/login', (req, res) => {
     // get all notification
     const response = { success: false };
     const { username, password } = req.body;
 
-    postLogin(undefined, username, password).then(({ data }) => {
-      const {
-        Success, Message, Token, errorCode,
-      } = parseJSONIfString(data);
+    postLogin(undefined, username, password)
+      .then(({ data }) => {
+        const { Success, Message, Token, errorCode } = parseJSONIfString(data);
 
-      res.set('Content-Type', 'application/json');
+        res.set('Content-Type', 'application/json');
 
-      if (Success) {
-        response.data = { token: Token, user: parseJSONIfString(Message) };
-        response.success = true;
-      } else {
-        response.message = Message;
-        response.errorCode = errorCode;
-      }
+        if (Success) {
+          response.data = { token: Token, user: parseJSONIfString(Message) };
+          response.success = true;
+        } else {
+          response.message = Message;
+          response.errorCode = errorCode;
+        }
 
-      res.send(response);
-    }).catch(({ message }) => res.send({
-      success: false,
-      message,
-    }));
+        res.send(response);
+      })
+      .catch(({ message }) =>
+        res.send({
+          success: false,
+          message
+        })
+      );
+  });
+
+  app.post('/signup', (req, res) => {
+    const response = { success: false };
+    const {
+      Username: username,
+      Password: password,
+      NIK: nik,
+      Telephone: phone,
+      Email: email,
+      Alamat: address
+    } = req.body;
+
+    postRegister(undefined, {
+      Username: username,
+      Password: password,
+      NIK: nik,
+      Telephone: phone,
+      Email: email,
+      Alamat: address
+    })
+      .then(({ data }) => {
+        const { Message: message, UserId: userID, Success: success } = data;
+
+        if (success) {
+          response.success = true;
+        } else {
+          response.message = message;
+        }
+
+        res.send(response);
+      })
+      .catch(({ message }) =>
+        res.send({
+          success: false,
+          message
+        })
+      );
+  });
+
+  app.post('/reset_password', (req, res) => {
+    const response = { success: false };
+    const { request: email } = req.body;
+
+    postRegister(undefined, {
+      request: email
+    })
+      .then(({ data }) => {
+        const { Message: message, data: userData, Success: success } = data;
+
+        if (success) {
+          response.success = true;
+          response.data = userData[0];
+        } else {
+          response.message = message;
+        }
+
+        res.send(response);
+      })
+      .catch(({ message }) =>
+        res.send({
+          success: false,
+          message
+        })
+      );
   });
 };
