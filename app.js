f4567852a8160c0912e23ef7ddcfeed0e1347971
Try Ajitiono <ballinst@gmail.com>
2018-12-09 00:05:51 +1100

    Implement CORS (#8)

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ global.Promise = require('bluebird');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Instantiate imported modules
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ // for parsing URL-encoded bodies
-  extended: true,
-}));
+app.use(
+  bodyParser.urlencoded({
+    // for parsing URL-encoded bodies
+    extended: true
+  })
+);
 
 // Export app
 module.exports = app;
